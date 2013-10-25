81bb945d143131a5d67794cc0a38be8437532848
Victoria Schdeva <victoria.schdeva@gmail.com>
2013-10-25 15:10:45 +0200

    Created a rails application and added it

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sham::Application.config.secret_token = '3b099fe9e89441c33facf46a4040019427eb96a67541836034f51f66ec1492d5b29e4489cd41e960159ca6e873779af55e5cc32b80846596224e79bc88a43d34'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Sham::Application.config.session_store :cookie_store, key: '_sham_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Sham::Application.config.session_store :active_record_store
